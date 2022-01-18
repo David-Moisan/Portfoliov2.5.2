36da9dab9b9b92c7a723523b41a5d0d41a048481
David-Moisan <dmoisan.23@gmail.com>
2022-01-18 17:38:13 +0100

    Refactoring: summernote

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,3 @@
-from re import template
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -7,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/auth', include('rest_framework.urls')),
-    path('summernote', include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('api/project/', include('project.urls')),
     path('api/blog/', include('blog.urls')),
     path('', include('frontend.urls')),
