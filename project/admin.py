6c5175cdca2a850c0290189adf0c1d7c17dd838c
David-Moisan <dmoisan.23@gmail.com>
2022-01-18 17:43:56 +0100

    Fix: summernote bug

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Language, Project
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
