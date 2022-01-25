1d3f98468289763e6c33af757f65a2c1bab0dde9
David-Moisan <dmoisan.23@gmail.com>
2022-01-25 08:31:04 +0100

    Feat: Adaptation des icones en fonction du viewport

--- frontend/src/components/components/Menu.js
+++ frontend/src/components/components/Menu.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useMediaQuery } from "react-responsive"
 
 export default function Menu() {
+    const isTablet = useMediaQuery({maxWidth: 768});
+
     return (
         <nav className='navbar__menu'>
-            <NavLink to="/project">Project</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/project" title='My Project'>{isTablet ? <i className="fas fa-project-diagram mobile-icons"></i> : "Project" }</NavLink>
+            <NavLink to="/about" title='About me'>{isTablet ? <i className="fas fa-user-circle mobile-icons"></i> : "About"}</NavLink>
+            {isTablet ? <NavLink to='/' title='Home'><i className='fas fa-home mobile-icons'></i></NavLink> : null }
+            <NavLink to="/blog" title='Blog Posts'>{isTablet ? <i className="fas fa-newspaper mobile-icons"></i> : "Blog"}</NavLink>
+            <NavLink to="/contact" title='Contact me'>{isTablet ? <i className="fas fa-paper-plane mobile-icons"></i> :"Contact"}</NavLink>
         </nav>
     )
 }
