d0dfff605d6e6a8caa42cd717415f4af9b87d76f
David-Moisan <dmoisan.23@gmail.com>
2023-05-01 09:38:28 +0200

    Refactoring: Change the text of About me after 2 years

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -34,15 +34,27 @@ export default function AboutPage() {
                                 <AnimLetter letter="y" />
                             </Title>
                             <AboutText>
-                                After a career in the French Army as a
-                                parachutist, I decided to retrain in the field
-                                of web development in 2020. I therefore resumed
-                                my studies with the WIS Toulouse school to
-                                validate a BTS Web and mobile web development as
-                                well as a Digital Project Manager license.
-                                Unfortunately, I did not find a work-study
-                                program to continue my studies in master's
-                                degree in computer engineering.
+                                After a career in the French army as a
+                                paratrooper, I decided to convert to the field
+                                of web development in 2020. So I resumed my
+                                studies with the WIS school in Toulouse to
+                                validate a BTS DWWM as well as a Project Manager
+                                License. I started a Master's degree in Computer
+                                Engineering in 2022 for a two-year work-study
+                                period, after having found a work-study program
+                                in the banking sector, I had a proposal for a
+                                permanent contract in the field of Insurance,
+                                which I immediately accepted. Indeed, living
+                                from my passion was my goal since the beginning
+                                of my retraining. So I stopped my Master in the
+                                course of the first year without validating it.
+                                And it's therefore in the field of insurance
+                                that I take up arms again, but this time sitting
+                                on an office chair in order to manage the EDI
+                                flows of a company wich generates millions in
+                                turnover. Here I'm finally full stack Web
+                                Developer in a technology that I particularity
+                                like PHP.
                             </AboutText>
                             <AboutText>
                                 Since I discovered IT development, I have never
